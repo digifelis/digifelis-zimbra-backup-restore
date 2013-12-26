93aec0a928323bc228efc65ea1f51438500c0221
digifelis <mansur@siirt.edu.tr>
2013-12-26 23:28:45 +0200

    Update zimbra_yedek_al.sh

--- zimbra_yedek_al.sh
+++ zimbra_yedek_al.sh
@@ -7,10 +7,10 @@ chmod 777 $NOW/sifreler.txt
 
 source /opt/zimbra/backup/zm.conf 
 
-for i in `zmprov -l gaa | egrep -v 'galsync|spam|ham|virus|stimpson'`;
+for i in `/opt/zimbra/bin/zmprov -l gaa | egrep -v 'galsync|spam|ham|virus|stimpson'`;
 do
-	echo $i , `zmprov -l ga $i userPassword | grep userPassword | sed 's/userPassword: //'` >> $WORKDIR/$NOW/sifreler.txt
-	echo "$i , `zmprov -l ga $i userPassword | grep userPassword | sed 's/userPassword: //'`";
+	echo $i , `/opt/zimbra/bin/zmprov -l ga $i userPassword | grep userPassword | sed 's/userPassword: //'` >> $WORKDIR/$NOW/sifreler.txt
+	echo "$i , `/opt/zimbra/bin/zmprov -l ga $i userPassword | grep userPassword | sed 's/userPassword: //'`";
 	curl -k -u $ADMINUSER:$ADMINPASS https://$URLAL:7071/home/$i/?fmt=tgz > $WORKDIR/$NOW/$i.tgz 
 	echo '------------------------------------------------------------------------------'
 done;
